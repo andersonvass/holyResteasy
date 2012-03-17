ffcbbcd5ae2fe5586bc0f9f703f92016ccb5955a
Anderson <anderson.vass@gmail.com>
2012-03-17 17:23:16 -0400

    Criacao do cadastro de usuarios

--- view/main-web/src/main/java/org/holy/holyResteasy/web/config/Application.java
+++ view/main-web/src/main/java/org/holy/holyResteasy/web/config/Application.java
@@ -3,11 +3,14 @@ package org.holy.holyResteasy.web.config;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.holy.holyResteasy.web.user.UserHB;
+
 public class Application extends javax.ws.rs.core.Application {
 
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(UserHB.class);
 		return classes;
 	}
 
